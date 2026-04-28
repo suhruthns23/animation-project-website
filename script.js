a13fbcd0215d1efa9ec6f090616730f7850149dd
suhruthns23 <suhruthns@gmail.com>
2026-04-28 23:56:34 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,11 +2,26 @@ function toggle(id) {
   let cie = document.getElementById("cie");
   let assign = document.getElementById("assign");
 
-  // Close both first
   cie.style.display = "none";
   assign.style.display = "none";
 
-  // Open selected one
   let selected = document.getElementById(id);
   selected.style.display = "flex";
+}
+
+// Sub dropdown toggle
+function subToggle(id) {
+  let all = document.querySelectorAll(".sub-dropdown");
+
+  all.forEach(el => {
+    if (el.id !== id) el.style.display = "none";
+  });
+
+  let current = document.getElementById(id);
+
+  if (current.style.display === "flex") {
+    current.style.display = "none";
+  } else {
+    current.style.display = "flex";
+  }
 }
