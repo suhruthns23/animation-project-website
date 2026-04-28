5242fa7f80a9dda03ea8a2c545d30782e78c0a5b
suhruthns23 <suhruthns@gmail.com>
2026-04-28 23:36:15 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,60 +1,12 @@
-body {
-  margin: 0;
-  font-family: Arial;
-  background: linear-gradient(to right, #141e30, #243b55);
-  color: white;
-}
+function toggle(id) {
+  let cie = document.getElementById("cie");
+  let assign = document.getElementById("assign");
 
-.container {
-  text-align: center;
-  padding: 30px;
-}
+  // Close both first
+  cie.style.display = "none";
+  assign.style.display = "none";
 
-/* HEADER CARD */
-.header-card {
-  background: rgba(255,255,255,0.1);
-  padding: 20px;
-  border-radius: 15px;
-  width: 80%;
-  margin: auto;
-  margin-bottom: 30px;
-  backdrop-filter: blur(10px);
-}
-
-.header-card h1 {
-  margin-bottom: 10px;
-}
-
-/* BUTTONS */
-button {
-  padding: 12px 25px;
-  margin: 15px;
-  cursor: pointer;
-  background: #ff4c4c;
-  border: none;
-  color: white;
-  font-size: 16px;
-  border-radius: 8px;
-}
-
-/* DROPDOWN */
-.dropdown {
-  display: none;
-  flex-direction: column;
-  align-items: center;
-}
-
-.dropdown a {
-  display: block;
-  padding: 10px;
-  width: 250px;
-  margin: 5px;
-  background: rgba(255,255,255,0.1);
-  border-radius: 8px;
-  color: white;
-  text-decoration: none;
-}
-
-.dropdown a:hover {
-  background: #ff4c4c;
+  // Open selected one
+  let selected = document.getElementById(id);
+  selected.style.display = "flex";
 }
